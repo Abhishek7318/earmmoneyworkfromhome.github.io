3ce44ef6685546f714942e73b8bf90634048e3a7
Abhishek7318 <127573710+Abhishek7318@users.noreply.github.com>
2023-08-06 15:21:25 +0530

    update

--- index.php
+++ index.php
@@ -88,9 +88,6 @@ mysqli_close($conn);
 
 ?>
 
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -100,7 +97,6 @@ mysqli_close($conn);
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     <title>PHP login system!</title>
   </head>
   <body>
